8d7642c6d5a2a7952274eeefdfe48773744c18e7
nursypat <77187968+nursypat@users.noreply.github.com>
2021-01-15 19:24:11 +0300

    init

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return HttpResponse("hello nunu")
+
+
+def test(request):
+    return render(request, "test.html")    
