0bf0847732a193986ce9c13657f8449bec989fbd
nursypat <77187968+nursypat@users.noreply.github.com>
2021-01-16 21:32:22 +0300

    added static urls and roots

--- todo/urls.py
+++ todo/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage, test, check, third
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path("check", check),
     path("test3", third, name = "test3")
    
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL,   document_root=settings.MEDIA_ROOT)
